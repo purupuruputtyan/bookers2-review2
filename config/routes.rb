5967d588a65b53969f0435612803cd26d9a99511
purupuruputtyan <ec2-user@ip-172-31-7-79.ap-northeast-1.compute.internal>
2023-03-30 11:29:42 +0000

    [Add]  ]ビューの追加、コントローラーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   root to: "homes#top"
-  get "home/about" => "homes/about", as: "about"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
+
+  get "home/about" => "homes#about", as: "about"
+
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
