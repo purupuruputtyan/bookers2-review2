c49cec3ae622f637c4fb297aff9d4006a095498d
purupuruputtyan <ec2-user@ip-172-31-7-79.ap-northeast-1.compute.internal>
2023-03-30 13:05:06 +0000

    [Add]STEP2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,8 +8,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -23,9 +27,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
