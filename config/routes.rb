4b11c23a0c1ba217294dce058a5081ca9122492e
purupuruputtyan <ec2-user@ip-172-31-7-79.ap-northeast-1.compute.internal>
2023-03-31 06:45:23 +0000

    [Add]いいね、コメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
-  root to: "homes#top"
   devise_for :users
-
-  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
-  resources :users, only: [:index, :show, :edit, :update]
-
+  root to: "homes#top"
   get "home/about" => "homes#about", as: "about"
+
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:index, :show, :edit, :update]
 
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
